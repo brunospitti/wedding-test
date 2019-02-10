92f03f17c4395f46eafff7cf7090c63ea3d8624d
Bruno Spitti <bruno.olivares@live.com>
2019-02-10 16:54:13 +0000

    favicon fixes

--- src/components/helpers/Layout.js
+++ src/components/helpers/Layout.js
@@ -25,25 +25,22 @@ const TemplateWrapper = ({ children }) => (
             name="description"
             content={data.site.siteMetadata.description}
           />
-
           <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/img/apple-touch-icon.png"
+            rel="shortcut icon"
+            href="/img/favicon.ico"
+            type="image/x-icon"
           />
-          <link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
-          <link rel="icon" href="/img/favicon.ico" type="image/x-icon">
-          <link
-            rel="mask-icon"
-            href="/img/safari-pinned-tab.svg"
-            color="#ff4400"
-          />
+          <link rel="icon" href="/img/favicon.ico" type="image/x-icon" />
           <meta name="theme-color" content="#fff" />
 
           <meta property="og:type" content="business.business" />
           <meta property="og:title" content={data.site.siteMetadata.title} />
           <meta property="og:url" content="/" />
           <meta property="og:image" content="/img/og-image.jpg" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
         </Helmet>
         <div>{children}</div>
       </div>
